07cb91e3de8a7ad74e9a1c31aaebef7654049206
Omar-debuging <eladnaniomar153@gmail.com>
2023-11-06 02:21:27 -0800

    matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,8 @@ def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             if j != len(matrix[i]) - 1:
-                    endspace = ' '
-                else:
-                    endspace = ''
+                endspace = ' '
+            else:
+                endspace = ''
             print("{:d}".format(matrix[i][j]), end=endscape)
         print()
