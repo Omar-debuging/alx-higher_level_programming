e549b26ff919b407ba8db8811286da0ad09a69da
Omar-debuging <eladnaniomar153@gmail.com>
2023-11-06 06:16:20 -0800

    matrix

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,24 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    l1 = list(tuple_a)
-    l2 = list(tuple_b)
-    result = []
-    if len(l1) >= 1:
-        x1 = l1[0]
-    else:
-        x1 = 0
-    if len(l1) >= 2:
-        x2 = l1[1]
-    else:
-        x2 = 0
-    if len(l2) >= 1:
-        y1 = l2[0]
-    else:
-        y1 = 0
-    if len(l2) >= 2:
-        y2 = l2[1]
-    else:
-        y2 = 0
-    result += [x1 + y1, x2 + y2]
-    x = tuple(result)
-    print(x)
+    len_a, len_b = len(tuple_a), len(tuple_b)
+    new_tuple = ((tuple_a[0] if len_a >= 1 else 0) +
+                 (tuple_b[0] if len_b >= 1 else 0),
+                 (tuple_a[1] if len_a >= 2 else 0) +
+                 (tuple_b[1] if len_b >= 2 else 0))
+    return new_tuple
