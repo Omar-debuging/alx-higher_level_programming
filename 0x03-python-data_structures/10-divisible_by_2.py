bd26881a58a6586a21087ab2980610a6b4f0e6ee
Omar-debuging <eladnaniomar153@gmail.com>
2023-11-06 12:52:09 -0800

    matrix

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     newlist = my_list.copy()
-    for i in my_list:
-        if i % 2 == 0:
-            newlist[i] = True
-        else:
-            newlist[i] = False
-    return newlist
+    if my_list:
+        for i in my_list:
+            if i % 2 == 0:
+                newlist[i] = True
+            else:
+                newlist[i] = False
+        return newlist
