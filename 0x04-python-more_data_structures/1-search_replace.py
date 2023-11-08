f90e13175e1a07c33eaa2321801e0ab0345a3bfb
Omar-debuging <eladnaniomar153@gmail.com>
2023-11-08 10:50:43 -0800

    matrix

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     newlist = []
-    for i in range (len(my_list)):
+    for i in range(len(my_list)):
         newlist.append(replace if my_list[i] == search else my_list[i])
     return newlist
