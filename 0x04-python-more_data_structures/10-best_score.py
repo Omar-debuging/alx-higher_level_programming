c5538702877e5adf84626d56f8e87566194344dd
Omar-debuging <eladnaniomar153@gmail.com>
2023-11-09 07:29:40 -0800

    more

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -6,6 +6,6 @@ def best_score(a_dictionary):
         for i in range(len(l)):
             if l[i][1] >= maximum:
                 maximum = l[i][1]
-        return list(a_dictionary.keys())[list(a_dictionary.values()).index(maximum)]
+        return (list(a_dictionary.keys())[list(a_dictionary.values()).index(maximum)])
     else:
         return None
