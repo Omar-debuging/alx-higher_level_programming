2b18b98bbf27a0ee658b85797e4a076333c6d813
Omar-debuging <eladnaniomar153@gmail.com>
2023-11-18 12:54:29 -0800

    advance

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+def complex_delete(a_dictionary, value):
+    dict = {}
+    if value not in a_dictionary.values():
+        return a_dictionary
+    else:
+        for key, val in a_dictionary.items():
+            if val != value:
+                dict[key] = val
+        return  dict
